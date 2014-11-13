9cbdf8bf85f6f8fe3153ffd7a8433a98486514af
Wagner <wgs06@msn.com>
2014-11-13 15:38:38 -0200

    consistência de cadastro, edição e listagem - controle user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,35 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user, 
+						:notice => 'Cadastro criado com sucesso!'
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to @user, :notice => 'Cadastro atualizado com sucesso!'
+		else
+			render :update
+		end
+	end
+
+	def user_params
+		params.require(:user).permit(:bio, :email, :full_name, :location, :password, 
+									 :password_confirmation)
+	end
 end
