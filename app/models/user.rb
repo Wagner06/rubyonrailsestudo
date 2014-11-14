f0f2bb968c0cbde04965592a8feebd0d90c0a6b3
Wagner <wgs06@msn.com>
2014-11-14 16:23:24 -0200

    layout (css) recurso quartos e cadastro

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 
-	validates :email, :full_name, :location, :password, presence: true
-	validates_confirmation_of :password
+	validates :email, :full_name, :location, presence: true
 	validates_length_of :bio, :minimum => 30, :allow_blank => false
 	validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 	validates_uniqueness_of :email 
+
+	has_secure_password
 end
